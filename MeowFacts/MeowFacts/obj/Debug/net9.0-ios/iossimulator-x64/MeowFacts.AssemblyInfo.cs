d75f17e0743abe1902530f2679a9d3c5833aee26
J180481 <J180481@tafe.wa.edu.au>
2026-06-08 18:07:27 +0800

    chore: added services folder
    added services folder for mvc structure

--- MeowFacts/MeowFacts/obj/Debug/net9.0-ios/iossimulator-x64/MeowFacts.AssemblyInfo.cs
+++ MeowFacts/MeowFacts/obj/Debug/net9.0-ios/iossimulator-x64/MeowFacts.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeowFacts")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+21e7279e23fac2c02abc2285f060ffae8aa83d0e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+fe96c1233deaaaf7f1228d0aef050807629a72a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeowFacts")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeowFacts")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
